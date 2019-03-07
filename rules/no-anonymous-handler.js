8b6f414688b0de465a0aec751a81691f4f494bce
Rahul9046 <rahul.das@fusioncharts.com>
2019-03-07 12:58:04 +0530

    add support for attachEvent in no-anonymous-handler-rule

--- rules/no-anonymous-handler.js
+++ rules/no-anonymous-handler.js
@@ -29,12 +29,12 @@ module.exports = {
             return {
                 "CallExpression"(node) {
                     // it checks whether the callee of the call expression has a property value and the expression was called
-                    // using two arguments. if both the conditions satisfy then check whether the property name is 'addEventListener'.
+                    // using two arguments. if both the conditions satisfy then check whether the property name is 'addEventListener' or 'attachEvent'.
                     // If true then check whether the second argument (which is the handler) is an anonymous. if any of the given 
                     // conditions do not satisfy then skip that node. 
-                    if (node.arguments.length !==2 || !node.callee.property || node.callee.property.name !== 'addEventListener' ||
-                    checkAnonymousHandler(node.arguments[1])){
-                      return;
+                    if (node.arguments.length !==2 || !node.callee.property || (node.callee.property.name !== 'addEventListener' &&
+                        node.callee.property.name !== 'attachEvent') || checkAnonymousHandler(node.arguments[1])){
+                            return;
                      }
                   context.report({
                   node: node.arguments[1],
